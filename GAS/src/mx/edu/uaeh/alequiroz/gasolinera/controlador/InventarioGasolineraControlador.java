9015a2d134bd90d6395baad8f25de9ae43af4643
AlesitaSpock <ale_jandra_098@hotmail.com>
2016-06-02 01:52:10 -0500

    n.n

---
+++ GAS/src/mx/edu/uaeh/alequiroz/gasolinera/controlador/InventarioGasolineraControlador.java
@@ -0,0 +1,38 @@
+package mx.edu.uaeh.alequiroz.gasolinera.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+
+public class InventarioGasolineraControlador {
+	@FXML
+	private TextField nombreProd;
+	
+	@FXML
+	private TextField costoUnitario;
+	
+	@FXML
+	private MenuButton cantidad;
+	
+	@FXML
+	private TextField existencias;
+	
+	
+	@FXML
+	private Button cancelarReg;
+	
+	@FXML
+	private Button agregarReg;
+	
+		
+	@FXML	
+    private void initialize() {
+		
+	
+	
+		
+	}
+	
+}
---
+++ GAS/src/mx/edu/uaeh/alequiroz/gasolinera/controlador/RegistrarGasolineraControlador.java
@@ -0,0 +1,32 @@
+package mx.edu.uaeh.alequiroz.gasolinera.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegistrarGasolineraControlador {
+
+	
+	@FXML
+	private TextField usuarioNuevo;
+	
+	@FXML
+	private TextField nombreUsuario;
+	
+	@FXML
+	private TextField contraseñaNueva;
+	
+	@FXML
+	private Button cancelarNuevo;
+	
+	@FXML
+	private Button agregarNuevo;
+	
+	@FXML
+	private void initialize(){
+		
+		
+	}
+	
+	
+}
